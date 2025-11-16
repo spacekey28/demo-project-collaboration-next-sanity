1df9389ef26f15927db83ed4c8a73df375e1b002
Peter Son <peteson.me@gmail.com>
2025-11-17 01:52:16 +1300

    fix: Update commit rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,7 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Allow any subject case (including capitalized first letter)
+    "subject-case": [0],
+  },
+};
